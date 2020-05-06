7a010c0843decefe53352ea537e7cf97d6f33f53
PuPuppru <zhengruru123@gmail.com>
2020-05-07 03:42:30 +0800

    do sth.

--- system_prop.mk
+++ system_prop.mk
@@ -10,6 +10,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.netflix.bsp_rev=Q845-05000-1
 
+# Radio
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true
+
 # Dalvik
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.heapstartsize=8m \
